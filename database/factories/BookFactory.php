331a6f436b81d9bcc5a0f84762ac98716e5c57a4
DeucesOUT1 <timmangoandre@gmail.com>
2024-07-27 11:46:53 +0800

    Made Book factory and gen 20 books

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class BookFactory extends Factory
+{
+    protected $model = Book::class;
+
+    public function definition()
+    {
+        return [
+            'isbn' => $this->faker->isbn13,
+            'title' => $this->faker->sentence,
+            'author' => $this->faker->name,
+            'description' => $this->faker->paragraph,
+            'date_published' => $this->faker->date,
+        ];
+    }
+}
