a0b8c3a119ab03d3fa8eb89a6c59542539a75ba5
unknown <primusr@M303.UC-BCF.EDU.PH>
2024-07-30 06:25:48 +0800

    Used Flowbite to edit tables

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,34 +1,65 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-2xl font-bold mb-4">Book List</h1>
-    @if(isset($query))
-        <p>Search results for: "{{ $query }}"</p>
-    @endif
-    <table class="min-w-full bg-white">
-        <thead class="bg-gray-800 text-white">
+<h1 class="text-2xl font-bold mb-4">Book List</h1>
+@if(isset($query))
+<p>Search results for: "{{ $query }}"</p>
+@endif
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th class="w-1/3 px-4 py-2">ISBN</th>
-                <th class="w-1/3 px-4 py-2">Title</th>
-                <th class="w-1/3 px-4 py-2">Author</th>
-                <th class="w-1/3 px-4 py-2">Description</th>
-                <th class="w-1/3 px-4 py-2">Date Published</th>
+                <th scope="col" class="px-6 py-3">
+                    ID
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    ISBN
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Title
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Author
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Description
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Date Published
+                </th>
             </tr>
         </thead>
         <tbody>
             @forelse ($books as $book)
-                <tr>
-                    <td class="border px-4 py-2">{{ $book->isbn }}</td>
-                    <td class="border px-4 py-2">{{ $book->title }}</td>
-                    <td class="border px-4 py-2">{{ $book->author }}</td>
-                    <td class="border px-4 py-2">{{ $book->description }}</td>
-                    <td class="border px-4 py-2">{{ $book->date_published }}</td>
-                </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {{ $book->id }}
+                </th>
+                <td class="px-6 py-4">
+                {{ $book->isbn }}
+                </td>
+                <td class="px-6 py-4">
+                {{ $book->title }}
+                </td>
+                <td class="px-6 py-4">
+                {{ $book->author }}
+                </td>
+                <td class="px-6 py-4">
+                {{ $book->description }}
+                </td>
+                <td class="px-6 py-4">
+                {{ $book->date_published }}
+                </td>
+            </tr>
             @empty
-                <tr>
-                    <td colspan="5" class="border px-4 py-2 text-center">No books found</td>
-                </tr>
+            <tr>
+                <td colspan="5" class="border px-4 py-2 text-center">No books found</td>
+            </tr>
             @endforelse
         </tbody>
     </table>
+</div>
 @endsection
+
+
+
